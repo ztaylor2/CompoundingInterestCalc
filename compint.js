86c48e3ab589fac6dbbf0de3f00799960fb00ed1
Zach Taylor <ztaylor2@zagmail.gonzaga.edu>
2017-02-02 20:15:45 -0800

    Update compint.js
    Add commas to output.

--- compint.js
+++ compint.js
@@ -4,15 +4,22 @@ var finalVal = function () {
 	var t = parseFloat(document.getElementById("years").value);
 	var n = parseFloat(document.getElementById("period").value);
 	var out = document.getElementById("final");
-	
+
+//add commas to output
+function numberWithCommas(x) {
+		var parts = x.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(".");
+}
+
 //make sure inputs are numbers
 if (isNaN(p) || isNaN(r) || isNaN(t))                         
 	{
 	out.textContent = "Invalid input, enter numbers only."
 	}
+//execute compounding interest equation
 else {
-		var A = p*(1+(r/100)/n)**(t*n);                       //execute compounding interest equation
-	out.textContent =  "$" + A.toFixed(2);                    //round answer to two decimal places and print 
+		var A = p*(1+(r/100)/n)**(t*n);
+	out.textContent =  "$" + numberWithCommas(A.toFixed(2));
 	}
-
 }
